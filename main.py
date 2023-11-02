d9bf637802b8f9510208bb16e1ac0dcd40daa9df
oleksandrtytko <124786943+oleksandrtytko@users.noreply.github.com>
2023-11-02 08:55:32 +0000

    Refactored code

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ def main():
     for json_file_name in glob('data/**/*.json', recursive=True):
         with open(json_file_name) as json_file:
             json_data = flatten(json.load(json_file), '_')
-            data_file = open(os.path.splitext(json_file_name)[0] + '.csv', 'w', newline='')
-            csv_writer = csv.writer(data_file)
+            csv_file = open(os.path.splitext(json_file_name)[0] + '.csv', 'w', newline='')
+            csv_writer = csv.writer(csv_file)
             csv_writer.writerow([data for data in json_data])
             csv_writer.writerow([json_data[data] for data in json_data])
-            data_file.close()
+            csv_file.close()
     pass
 
 
